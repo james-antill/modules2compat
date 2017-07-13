76f045dbe6e0c70f341959118dfbd04858aaa97a
James Antill <james@and.org>
2017-07-12 23:18:18 -0400

    Fixup SELinux labels.

--- publish.sh
+++ publish.sh
@@ -12,6 +12,10 @@ fi
 
 for i in '' '-nodejs' '-rawhide'; do
  cp -a fedora-compat-modular$i $1
+ if [ -x /bin/chcon ]; then
+   chcon -R -t httpd_sys_content_t $1/fedora-compat-modular$i
+ fi
+
  mv $1/fedora-modular$i $1/fedora-modular$i.old
  mv $1/fedora-compat-modular$i $1/fedora-modular$i
  rm -rf $1/fedora-modular$i.old
